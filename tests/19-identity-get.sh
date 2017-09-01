c7727fc22c95d04b1e8f736bc0c5b2753887010f
Ian Vernon <ian@covalent.io>
2017-09-01 14:18:00 -0400

    tests: change runtime tests to use spaces instead of tabs
    Signed-off by: Ian Vernon <ian@cilium.io>

--- tests/19-identity-get.sh
+++ tests/19-identity-get.sh
@@ -5,20 +5,20 @@
 source "./helpers.bash"
 
 function start_containers {
-    docker run -dt --net=$TEST_NET --name foo -l id.foo tgraf/netperf
-    docker run -dt --net=$TEST_NET --name bar -l id.bar tgraf/netperf
-    docker run -dt --net=$TEST_NET --name baz -l id.baz tgraf/netperf
+  docker run -dt --net=$TEST_NET --name foo -l id.foo tgraf/netperf
+  docker run -dt --net=$TEST_NET --name bar -l id.bar tgraf/netperf
+  docker run -dt --net=$TEST_NET --name baz -l id.baz tgraf/netperf
 }
 
 function remove_containers {
-    docker rm -f foo foo bar baz 2> /dev/null || true
+  docker rm -f foo foo bar baz 2> /dev/null || true
 }
 
 function restart_cilium {
-    echo "------ restarting cilium ------"
-    service cilium restart
-    echo "------ waiting for cilium agent get up and running ------"
-    wait_for_cilium_status
+  echo "------ restarting cilium ------"
+  service cilium restart
+  echo "------ waiting for cilium agent get up and running ------"
+  wait_for_cilium_status
 }
 
 function cleanup {
@@ -41,34 +41,34 @@ logs_clear
 # The test launches three containers and waits until 3 endpoints are created in Cilium. It then extracts the security ID
 # from the `cilium endpoint list` output.
 function test_identity_get {
-    remove_containers
-    restart_cilium
-    start_containers
-    wait_for_endpoints 3
-    cilium endpoint list
-    ID=$(cilium endpoint list | grep id.foo | awk '{print $3}')
+  remove_containers
+  restart_cilium
+  start_containers
+  wait_for_endpoints 3
+  cilium endpoint list
+  ID=$(cilium endpoint list | grep id.foo | awk '{print $3}')
 
-    # Get expected response and replace all newline chars with a single space.
-    response=$(cilium identity get $ID | sed ':a;N;$!ba;s/\n/ /g')
+  # Get expected response and replace all newline chars with a single space.
+  response=$(cilium identity get $ID | sed ':a;N;$!ba;s/\n/ /g')
 
-    # Extract SHA256 value from response
-    str=$response
-    substring="labelsSHA256\": \""
-    len=${#substring}
+  # Extract SHA256 value from response
+  str=$response
+  substring="labelsSHA256\": \""
+  len=${#substring}
 
-    # Find the index of first occurrence of substring in response str
-    index=$(awk -v a="$str" -v b="$substring" 'BEGIN{print index(a,b)}')
-    substring=${str:index + len - 1}
-    sha256="$( cut -d '"' -f 1 <<< "$substring" )"
+  # Find the index of first occurrence of substring in response str
+  index=$(awk -v a="$str" -v b="$substring" 'BEGIN{print index(a,b)}')
+  substring=${str:index + len - 1}
+  sha256="$( cut -d '"' -f 1 <<< "$substring" )"
 
-    echo "Endpoint security ID is: $ID"
-    expected_response='{   "Payload": {     "id": '$ID',     "labels": [       "container:id.foo"     ],     "labelsSHA256": "'$sha256'"   } }'
+  echo "Endpoint security ID is: $ID"
+  expected_response='{   "Payload": {     "id": '$ID',     "labels": [       "container:id.foo"     ],     "labelsSHA256": "'$sha256'"   } }'
 
-    echo "Response is $response"
+  echo "Response is $response"
 
-    if [[ "${expected_response}" != "${response}" ]]; then
-        abort "Expected: ${expected_response}; Got: ${response}"
-    fi
+  if [[ "${expected_response}" != "${response}" ]]; then
+    abort "Expected: ${expected_response}; Got: ${response}"
+  fi
 }
 
 cilium endpoint list
